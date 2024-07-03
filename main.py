350b18781ca557ea340e350917885694f1ee9306
wvanzwol27 <wvanzwol27@my.whitworth.edu>
2024-07-02 18:56:54 -0600

    better documentation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 
 bot = Client(intents=Intents.GUILD_MEMBERS | Intents.DEFAULT)
 
+# When the discord bot is read it will do all in this function
 @listen()
 async def on_ready():
     print("Ready")
@@ -52,19 +53,33 @@ async def on_ready():
 
     await simulate_member_add()
 
+# When Member joins
 @listen(MemberAdd)
 async def an_event_handler(event: MemberAdd):
+
     print(f"Someone joined with name: {event.member.user.username}")
+
     # Create a category for the new member named after then
     category = await bot.guilds[0].create_category(name=event.member.user.username)
+
     # Create a channel for the new member called public in the category
     public_channel = await bot.guilds[0].create_text_channel(name="public", category=category)
     private_channel = await bot.guilds[0].create_text_channel(name="private", category=category)
     
-    # TODO: Adjust the permissions of the public and private channels
+    '''
+    Send messages is off by default except the member who joined.
+    View Channels is off by default except the member who joined and the public channel.
+    '''
+    # TODO: set permissions for category and channels
 
-    
+# TODO: `/create_channel` Slash Command
+# TODO: `/rename_channel` Slash Command
+# TODO: `/delete_channel` Slash Command
+# TODO: `/follow`
+# TODO: `/unfollow`
+# TODO: `/generate_rss`
 
+# Example slash command
 @slash_command(name='ping')
 async def ping(ctx):
     await ctx.send('Pong')
